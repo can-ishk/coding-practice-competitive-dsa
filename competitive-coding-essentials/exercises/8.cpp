2d72e2882ff0884d71be19ec5af10d409892c85a
Kanishk <kchathley26@gmail.com>
2022-06-15 22:39:14 +0530

    .

---
+++ competitive-coding-essentials/exercises/8.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+vector<int> smallestRange(vector<vector<int>> nums) {
+    int n = nums.size();
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
