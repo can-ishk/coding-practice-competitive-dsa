8488d6e193b23f46735d8c242462901d149eac06
Kanishk <kchathley26@gmail.com>
2022-08-07 00:54:13 +0530

    cses apartments

--- cses/SortingAndSearching/2-Apartments.cpp
+++ cses/SortingAndSearching/2-Apartments.cpp
@@ -5,6 +5,16 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    int n,m,k;
+    cin>>n>>m>>k;
+    vector<int> a(n);
+    vector<int> b(m);
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    for(int i=0;i<m;i++)
+        cin>>b[i];
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
     
     return 0;
 }
