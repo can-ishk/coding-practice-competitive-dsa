9f6545d5a402a0f409deec8bd0ebd65508d729b4
Kanishk <kchathley26@gmail.com>
2022-09-19 16:43:28 +0530

    Kickstart 2022 F I GIVE UP. Gonna go sleep, TOO SICK FOR THIS RIP. The questions are great tho.

---
+++ kickstart/2022/round-F/WaterContainerSystem.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    for(int c=1; c<=t; c++){
+        int count=0;
+        
+        cout<<"Case #"<<c<<": "<<count<<"\n";
+    }
+    return 0;
+}
